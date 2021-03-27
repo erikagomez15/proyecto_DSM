3bc7ec6c01ab8467812f2a15ff2689d43fdb9b16
kfuentes466 <36283211+kfuentes466@users.noreply.github.com>
2021-03-27 00:20:11 -0600

    Estilo de login terminado

--- dentalApp/app/src/main/java/sv/edu/udb/dentalapp/MainActivity.java
+++ dentalApp/app/src/main/java/sv/edu/udb/dentalapp/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, dashboard.class);
+                Intent intent = new Intent(MainActivity.this, login.class);
                 startActivity(intent);
                 finish();
             }
