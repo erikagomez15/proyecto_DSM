5039b46f3f24aeb694dae3a0f6d07312b8a21377
kfuentes466 <36283211+kfuentes466@users.noreply.github.com>
2021-03-28 22:02:29 -0600

    pantalla de recuperar contraseña mejorada

--- dentalApp/app/src/main/java/sv/edu/udb/dentalapp/login.java
+++ dentalApp/app/src/main/java/sv/edu/udb/dentalapp/login.java
@@ -3,10 +3,12 @@ package sv.edu.udb.dentalapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
+import  android.content.Intent;
 public class login extends AppCompatActivity {
 
     @Override
@@ -15,4 +17,17 @@ public class login extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
     }
+
+    public void forgetPassword(View view ){
+        Intent intent = new Intent(this,forgetPassword.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void signUp(View view){
+        Intent intent = new Intent(this, signUp.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
