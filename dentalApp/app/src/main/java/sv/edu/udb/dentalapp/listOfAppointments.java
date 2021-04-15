5ed79708a1684f11dc9dfff252470a315a29414a
kfuentes466 <36283211+kfuentes466@users.noreply.github.com>
2021-04-14 22:13:52 -0600

    Pantalla ver citas completa.

---
+++ dentalApp/app/src/main/java/sv/edu/udb/dentalapp/listOfAppointments.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.dentalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listOfAppointments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_appointments);
+    }
+}
